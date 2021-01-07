618c600b0d9ed49a738b63528496e3d66da64eb3
Shreya Agarwal <43489853+shreyaag770@users.noreply.github.com>
2021-01-08 03:18:15 +0530

    Update Highfivepage.js

--- src/Main/Highfivepage.js
+++ src/Main/Highfivepage.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-
+import { Link } from "react-router-dom";
 
 
 
@@ -11,12 +11,17 @@ const Highfive = () => {
   return (
     <div>
       <div>Do you want to send Highfive ?</div>
+      <Link to="/">
         <button>
           Yes
         </button>
+        </Link>
+        <Link to="/">
        <button>
            No
         </button>
+        </Link>
+
 
     </div>
  

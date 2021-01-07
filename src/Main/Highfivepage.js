fff59f6728cc9b79d915f2187feb7808c42cf13e
Shreya Agarwal <43489853+shreyaag770@users.noreply.github.com>
2021-01-08 03:08:51 +0530

    Add files via upload

---
+++ src/Main/Highfivepage.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+
+
+
+const Highfive = () => {
+
+
+  
+  return (
+    <div>
+      <div>Do you want to send Highfive ?</div>
+        <button>
+          Yes
+        </button>
+       <button>
+           No
+        </button>
+
+    </div>
+ 
+
+
+    
+  );
+};
+
+export default Highfive;
---
+++ src/Main/praisePage.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+
+
+
+const Praise = () => {
+
+
+  
+  return (
+    <div>
+<div>Do you want to praise ?</div>
+  <button>
+    Yes
+  </button>
+  <button>
+    No
+  </button>
+    </div>
+  
+
+
+
+    
+  );
+};
+
+export default Praise;
